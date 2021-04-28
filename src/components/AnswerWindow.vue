9e4475c92fda556b5d60b767a3c6428000688c63
Jeremy Eissler <jeissler0@gmail.com>
2021-04-28 12:30:50 -0500

    adds window bg elem for effect

--- src/components/AnswerWindow.vue
+++ src/components/AnswerWindow.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <h1>
+    <canvas id="canvas" class="absolute h-48 w-56 top-1/4" />
+    <h1 class="z-10 p-14 pt-16">
       {{ randomAnswer }}
     </h1>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { onMounted, computed } from 'vue';
 import answers from '../logic/answers.json';
 
 function getRandomIntInclusive(rawMin, rawMax) {
@@ -15,6 +16,20 @@ function getRandomIntInclusive(rawMin, rawMax) {
   const max = Math.floor(rawMax);
   // The maximum is inclusive and the minimum is inclusive
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function drawTriangle() {
+  const canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(300, 0);
+    ctx.lineTo(150, 150);
+    ctx.fillStyle = "#3f33ca";
+    ctx.fill();
+  }
 }
 
 export default {
@@ -27,8 +42,10 @@ export default {
       return allAnswers[randomIndex];
     });
 
+    onMounted(() => drawTriangle());
+
     return {
-      randomAnswer,
+      randomAnswer
     };
   },
 };
